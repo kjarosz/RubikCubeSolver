c034945265b09fb12c38be91cdb5731c8002de9f
kjarosz <kjarosz19946@gmail.com>
2015-01-25 04:07:14 -0500

    Implement a way to reverse algorithm.

--- src/solver/algorithms/Algorithm.java
+++ src/solver/algorithms/Algorithm.java
@@ -27,5 +27,24 @@ public class Algorithm {
       moves = new LinkedList<>(algorithm.moves);
       moves.addFirst(newMove);
    }
-    
+   
+   public Algorithm getReverseAlgorithm() {
+      Algorithm algorithm = new Algorithm(cubeState);
+      
+      Move values[] = Move.values();
+      for(Move move: moves) {
+         int ordinal = move.ordinal();
+         if(ordinal % 2 == 0) {
+            algorithm.moves.addFirst(values[ordinal+1]);
+         } else {
+            algorithm.moves.addFirst(values[ordinal]);
+         }
+      }
+      
+      for(Move move: algorithm.moves) {
+         algorithm.cubeState.performTransform(move);
+      }
+      
+      return algorithm;
+   }
 }
